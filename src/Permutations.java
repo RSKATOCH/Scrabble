ec6635d28664de4655b5c8bdb4eba42a2af4049d
Vivek Gupta <vivekgupta@paypal.com>
2016-08-05 14:48:39 +0530

    Fixed Permutation and Scrabble

--- src/Permutations.java
+++ src/Permutations.java
@@ -1,33 +1,38 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 
 	public class Permutations {
 	    public static Set<String> permutationFinder(String str) {
-	        Set<String> perm = new HashSet<String>();
-	        
-	        if (str == null) {
-	            return null;
-	        } else if (str.length() == 0) {
-	            perm.add("");
-	            return perm;
-	        }
-	        char initial = str.charAt(0); 
-	        String rem = str.substring(1); 
-	        Set<String> words = permutationFinder(rem);
-	        for (String strNew : words) {
-	            for (int i = 0;i<=strNew.length();i++){
-	                perm.add(charInsert(strNew, initial, i));
-	            }
-	        }
-	        return perm;
+	    	Set<String> words = new HashSet<String>();
+
+			for (int i = 0; i < str.length(); i++) {
+
+				words = addLetter(str.charAt(i), words);
+			}
+
+			return words;
 	    }
 
-	    public static String charInsert(String str, char c, int j) {
-	        String begin = str.substring(0, j);
-	        String end = str.substring(j);
-	        return begin + c + end;
-	    }
+	    private static Set<String> addLetter(char charAt, Set<String> words) {
+			Set<String> newWords = new HashSet<String>();
+			for (String word : words) {
+				for (int i = 0; i < word.length(); i++) {
+					StringBuilder str = new StringBuilder(word);
+					newWords.add(getSorted(str.insert(i, charAt).toString()));
+				}
+			}
+			newWords.add(String.valueOf(charAt));
+			newWords.addAll(words);
+			return newWords;
+		}
+	    
+	    private static String getSorted(String word) {
+			char[] wordArray = word.toCharArray();
+			Arrays.sort(wordArray);
+			return String.valueOf(wordArray);
+		}
 	    
 	}
 
--- src/Scrabble.java
+++ src/Scrabble.java
@@ -1,93 +1,121 @@
-import java.util.ArrayList;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Scrabble {
-	        
-			String letters;
-			
-			public static ArrayList<String> readFile(String path) throws Exception {
-				return (ArrayList<String>) Files.readAllLines(Paths.get(path));
+
+	String letters;
+	final String BLANK = "&";
+
+	private String getSorted(String word) {
+		char[] wordArray = word.toCharArray();
+		Arrays.sort(wordArray);
+		return String.valueOf(wordArray);
+	}
+	
+	public Set<String> getWordSet(ArrayList<String> wordsList) {
+		Set<String> wordSet = new HashSet<String>();
+		for(String word : wordsList) {
+			wordSet.add(getSorted(word));
+		}
+		return wordSet;
+	}
+	
+	public Set<String> readFile(String path) throws Exception {
+		return getWordSet((ArrayList<String>) Files.readAllLines(Paths.get(path)));
+	}
+
+	public boolean isValid(String s, Set<String> dict, int numOfBlanks) {
+		String regstr = s.replaceAll("", ".*");
+		for(String word : dict) {
+			if(word.matches(regstr) && (word.length() - s.length() == numOfBlanks)) {
+				System.out.println(word);
+				return true;
 			}
-			
-			public boolean isValid(String s, ArrayList<String> dict){
-				return dict.contains(s);
+		}
+		
+		return false;
+	}
+
+	public int scoreLetter(char c) {
+		switch (c) {
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':
+		case 'L':
+		case 'N':
+		case 'R':
+		case 'S':
+		case 'T':
+			return 1;
+		case 'D':
+		case 'G':
+			return 2;
+		case 'B':
+		case 'C':
+		case 'M':
+		case 'P':
+			return 3;
+		case 'F':
+		case 'H':
+		case 'W':
+		case 'Y':
+		case 'V':
+			return 4;
+		case 'K':
+			return 5;
+		case 'J':
+		case 'X':
+			return 8;
+		case 'Z':
+		case 'Q':
+			return 10;
+		default:
+			return 0;
+		}
+
+	}
+	
+	
+
+	public int totalScore(String s) {
+		int score = 0;
+		for (int i = 0; i < s.length(); i++)
+			score += scoreLetter(s.charAt(i));
+		return score;
+	}
+	
+	public int getMaxScore(String str, Set<String> dictionary) {
+		int maxScore = 0;
+		String sortedStrWithoutBlank = removeBlanksSorted(str);
+		Set<String> permutations = Permutations.permutationFinder(str);
+		int numOfBlanks = str.length() - sortedStrWithoutBlank.length();
+		
+		System.out.println(sortedStrWithoutBlank);
+		
+		for(String permWord : permutations) {
+			if(isValid(permWord, dictionary, numOfBlanks) && maxScore < totalScore(permWord)) {
+				maxScore = totalScore(permWord);
 			}
-			
-			public int scoreLetter(char c){
-				switch(c){
-					case 'A':
-					case 'E':
-					case 'I':
-					case 'O':
-					case 'U':
-					case 'L':
-					case 'N':
-					case 'R':
-					case 'S':
-					case 'T': 
-						return 1;
-					case 'D':
-					case 'G': 
-						return 2;
-					case 'B':
-					case 'C':
-					case 'M':
-					case 'P': 
-						return 3;
-					case 'F':
-					case 'H':
-					case 'W':
-					case 'Y':
-					case 'V': 
-						return 4;
-					case 'K': 
-						return 5;
-					case 'J':
-					case 'X': 
-						return 8;
-					case 'Z': 
-					case 'Q': 
-						return 10;
-					default:
-						return 0;
-				}
-					
-			}
-			
-				public int totalScore(String s){
-					int score=0;
-					for(int i=0;i<s.length();i++)
-						score += scoreLetter(s.charAt(i));
-					return score;
-				}
-				
-				public int maxScore(ArrayList<String> dict){
-					String temp = this.letters;
-					int maxScore=0;
-					Set<String> permute;
-				
-					for(int i=0;i<len;i++){
-					permute = Permutations.permutationFinder(temp);
-						for(int k=0;k<permute.size();k++){
-						maxScore = totalScore(temp);
-							if(isValid(temp, dict))
-								return maxScore;
-						}
-					return maxScore;
-				}
-			
-			
-			public static void main(String args[]){
-				string input = "schools";
-				String filePath = "C:\\Users\\subhargava\\Downloads\\sowpods.txt";
-				ArrayList<String> dictionary = new ArrayList<String> ();
-				dictionary = readFile(filePath);
-				
-				
-				
-			}
+		}
+		return maxScore;
+	}
+	
+	
+	private String removeBlanksSorted(String str) {
+		return getSorted(str.replaceAll(BLANK, ""));
+	}
+
+	public static void main(String args[]) throws Exception {
+		Scrabble sc = new Scrabble();
+		String filePath = args[0];
+		String input = args[1];
+		Set<String> dictionary = sc.readFile(filePath);
+		System.out.println(sc.getMaxScore(input, dictionary));		
+	}
 }
